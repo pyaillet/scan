3e230902e57d364cd39c2da5ff0b61fd2b7f5a27
Pierre-Yves Aillet <pierreyves.aillet@free.fr>
2016-02-05 06:50:57 +0000

    Double press to scan

--- scan.py
+++ scan.py
@@ -10,59 +10,76 @@ import smtplib
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 
+class Holder:
+  flag = False
+  def setUp(self):
+    self.flag = True
+  def setDown(self):
+    self.flag = False
+  def isUp(self):
+    return self.flag
 
 def sleeper():
-	while True:
-		time.sleep(60)
+  while True:
+    time.sleep(60)
+    holder.setDown()
 
 def send_email(config, file, to):
-	smtp = config.get('mail', 'smtp')
-	to = config.get('mail', to)
-	mailfrom = config.get('mail', 'from')
-	subject = config.get('mail', 'subject')
-	preamble = config.get('mail', 'preamble')
-	msg = MIMEMultipart()
-	msg['Subject'] = subject
-	msg['From'] = mailfrom
-	msg['To'] = to
-	msg.preamble = preamble
-	fp = open(file, 'rb')
-	img = MIMEImage(fp.read())
-	fp.close()
-	msg.attach(img)
-	s = smtplib.SMTP(smtp)
-	print "Sending mail..."
-	s.sendmail(mailfrom, to, msg.as_string())
-	s.quit()
-	print "Mail sent"
+  smtp = config.get('mail', 'smtp')
+  to = config.get('mail', to)
+  mailfrom = config.get('mail', 'from')
+  subject = config.get('mail', 'subject')
+  preamble = config.get('mail', 'preamble')
+  msg = MIMEMultipart()
+  msg['Subject'] = subject
+  msg['From'] = mailfrom
+  msg['To'] = to
+  msg.preamble = preamble
+  fp = open(file, 'rb')
+  img = MIMEImage(fp.read())
+  fp.close()
+  msg.attach(img)
+  s = smtplib.SMTP(smtp)
+  print "Sending mail..."
+  s.sendmail(mailfrom, to, msg.as_string())
+  s.quit()
+  print "Mail sent"
 
 def scan(config, file):
-	device = config.get('scanner', 'device')
-	scanimage = config.get('scanner', 'scanimage')
-	pnmtojpeg = config.get('scanner', 'pnmtojpeg')
-	cmd = "%s -d %s | %s --quality=100 > %s" % (scanimage, device, pnmtojpeg, file)
-	print "Scanning image..."
-	print cmd
-	os.system(cmd)
-	print "Image scanned"
+  device = config.get('scanner', 'device')
+  scanimage = config.get('scanner', 'scanimage')
+  pnmtojpeg = config.get('scanner', 'pnmtojpeg')
+  cmd = "%s -d %s | %s --quality=100 > %s" % (scanimage, device, pnmtojpeg, file)
+  print "Scanning image..."
+  print cmd
+  os.system(cmd)
+  print "Image scanned"
 
 def scan_and_send_to1(channel):
-	scan_and_send('to1')
+  if holder.isUp():
+    scan_and_send('to1')
+  else:
+    holder.setUp()
 
 def scan_and_send_to2(channel):
-	scan_and_send('to2')
+  if holder.isUp():
+    scan_and_send('to2')
+  else:
+    holder.setUp()
 
 def scan_and_send(to):
-	tmpdir = tempfile.mkdtemp()
-#	tmpdir = "/home/pi/scan"
-	tmpfile = "%s/scan.jpg" % (tmpdir)
-	try:
-		scan(config, tmpfile)
-		send_email(config, tmpfile, to)
-	finally:
-		print "Cleaning up..."
-		shutil.rmtree(tmpdir)
-		print "Done"
+  tmpdir = tempfile.mkdtemp()
+  #	tmpdir = "/home/pi/scan"
+  tmpfile = "%s/scan.jpg" % (tmpdir)
+  try:
+    scan(config, tmpfile)
+    send_email(config, tmpfile, to)
+    finally:
+      print "Cleaning up..."
+      shutil.rmtree(tmpdir)
+      print "Done"
+    
+holder = Holder()
 
 config = ConfigParser.RawConfigParser()
 config.read('scan.conf')
@@ -74,6 +91,6 @@ GPIO.add_event_detect(17, GPIO.FALLING, callback=scan_and_send_to1, bouncetime=3
 GPIO.add_event_detect(23, GPIO.FALLING, callback=scan_and_send_to2, bouncetime=300)  
 
 try:
-	sleeper()
+  sleeper()
 finally:
-	GPIO.cleanup()
+  GPIO.cleanup()
